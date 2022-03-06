7403ba9477a6ad2ba27af0a2d11843948ef65b72
Naveen Singisetty <naveens.in@mouritech.com>
2022-03-06 23:15:07 +0530

    Added SwiftLib.podspec

---
+++ SwiftLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftLib.swift
+//  SwiftLib
+//
+//  Created by Naveen Singisetty on 06/03/22.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
