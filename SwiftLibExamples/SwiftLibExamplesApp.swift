6525bb11933f4fd25be2b31e0ba80e840cc36f1b
Naveen Singisetty <naveens.in@mouritech.com>
2022-03-06 22:46:44 +0530

    Initial project setup

---
+++ SwiftLibExamples/SwiftLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLibExamplesApp.swift
+//  SwiftLibExamples
+//
+//  Created by Naveen Singisetty on 06/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
